9f6120fe3c2f200b0d3715973bc2ce34b2551455
Aoekae <dark_aoekae@yahoo.com>
2022-08-18 09:28:23 -0500

    Finished the base of the printJobs Method

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -120,6 +120,45 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+//for each job in the arraylist, iterate through the hashmap at that index and print the jobs
+        if (someJobs.size() > 0) {
+
+            for (int index = 0; index < someJobs.size(); index++) {
+                System.out.println("*****");
+                for (Map.Entry<String, String> jobs : someJobs.get(index).entrySet()) {
+
+                    System.out.println(jobs.getKey() + ": " + jobs.getValue());
+
+                }
+                System.out.println("*****\n");
+            }
+        } else {
+            System.out.println("No Results");
+        }
+
+        //Tip
+        //To do this, you’ll need to iterate over an ArrayList of jobs. Each job is itself a HashMap. While you can get
+        //each of the items out of the HashMap using the known keys (employer, location, etc.), think instead about
+        // creating a nested loop to loop over each HashMap. If a new field is added to the job records, this approach
+        // will print out the new field without any updates to printJobs.
     }
 }
+
+//Write the code for the task, verifying manually that it works by running the TechJobs.main method.
+//When you think you’ve completed a task, run the individual test that corresponds to the task.
+//If the test fails, review the test output and go back to your code to try to fix it.
+//Once the single test passes, run all of the tests to make sure you didn’t break any tests that previously passed.
+//Repeat this process until all tests pass.
+
+//For the autograding script to correctly grade your code, you’ll need to match this format exactly. In particular,
+// note the number of asterisks surrounding each listing, and the blank line between listings.
+//
+//If there are no results, it should print No Results. Again, you should use this exact message.
+//
+//*****
+//        position type: Web - Back End
+//        name: Ruby specialist
+//        employer: LaunchCode
+//        location: Saint Louis
+//        core competency: Javascript
+//        *****
